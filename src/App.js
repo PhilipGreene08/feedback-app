afb63df804f042299374edd9b95eab15f86a1fca
Phil Greene <philgreene08@gmail.com>
2021-12-12 22:40:08 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Fragment } from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <div className='container'>
+        <h1>My Lol</h1>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
