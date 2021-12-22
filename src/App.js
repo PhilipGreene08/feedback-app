b8b18a3d2c3b675b5db10470b5a91bd45b629843
Phil Greene <philgreene08@gmail.com>
2021-12-21 21:02:38 -0600

    8th day commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { Fragment } from 'react';
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackList from './components/FeedbackList';
 import Header from './components/Header';
 import FeedbackData from './data/FeedbackData';
 import FeedbackForm from './components/FeedbackForm';
+import AboutPage from './pages/AboutPage';
+import { v4 as uuidv4 } from 'uuid';
 //import Card from './components/shared/Card';
 
 function App() {
   const [feedback, setfeedback] = useState(FeedbackData);
 
   const addFeedback = (newFeedback) => {
-    console.log(newFeedback);
+    newFeedback.id = uuidv4();
+    setfeedback([newFeedback, ...feedback]);
   };
 
   const deleteFeedback = (id) => {
@@ -20,14 +24,29 @@ function App() {
     }
   };
   return (
-    <Fragment>
+    <Router>
       <Header />
-      <FeedbackForm handleAdd={addFeedback} />
       <div className='container'>
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            exact
+            path='/'
+            element={
+              <Fragment>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </Fragment>
+            }
+          ></Route>
+
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
       </div>
-    </Fragment>
+    </Router>
   );
 }
 
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,17 +1,40 @@
 import PropTypes from 'prop-types';
 import Feedbackitem from './Feedbackitem';
+import { motion, AnimatePresence } from 'framer-motion';
 
 function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>;
   }
+
   return (
     <div className='feedback-list'>
-      {feedback.map((item) => (
-        <Feedbackitem key={item.id} item={item} handleDelete={handleDelete} />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <Feedbackitem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
+
+  // return (
+  //   <div className='feedback-list'>
+  //     {feedback.map((item) => (
+  //       <Feedbackitem key={item.id} item={item} handleDelete={handleDelete} />
+  //     ))}
+  //   </div>
+  // );
 }
 
 FeedbackList.propTypes = {
